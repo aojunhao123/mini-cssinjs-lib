04dae9a8cb1e7fd58eaa2578e13fec10cbaa3d83
小豪 <1844749591@qq.com>
2025-01-10 22:54:49 +0800

    initial commit

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+import { css } from "../index";
+
+describe("css", () => {
+  it("should be a function", () => {
+    expect(typeof css).toBe("function");
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function css(
+  styles: string | TemplateStringsArray,
+  ...interpolations: any[]
+): string {
+  // 暂时返回一个空字符串，我们稍后会实现具体逻辑
+  return "";
+}
